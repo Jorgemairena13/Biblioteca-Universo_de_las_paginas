3227a2902347b874722e50085ac8f5462097ff11
Jorge <jorgemairena13@gmail.com>
2025-03-20 21:05:59 +0100

    Empezar la parte de prestamos

--- ARKANA.py
+++ ARKANA.py
@@ -34,32 +34,61 @@ class Cliente():
         self.correo_electronico = correo_electronico
 
 
+class Prestamo():
+
+    def __init__(self,dni , nombre,nombre_libro, fecha_prestamo, fecha_devolucion):
+        self.dni = dni
+        self.usuario = nombre
+        self.libro = nombre_libro
+        self.fecha_prestamo = fecha_prestamo
+        self.fecha_devolucion = fecha_devolucion
+
+
 class Biblioteca():
 
     def __init__(self):
         # Diccionarios donde se va a ir guardando todo
         self.libros = {}
         self.clientes = {}
+        self.prestados = {}
+        
+    #Funcion para añadir libro -------------------------------------------------------------------------------------------------
+    def añadir_libro(self,nombre,editorial,autor,fecha_publi,isbn):
+        
+        # Comprobamos que el libro no se duplique
+        if nombre not in self.libros:
+            #Creamos una insancia del libro y la guardamos con la clave del nombre del libro
+            self.libros[nombre] = Libro(nombre,editorial,autor,fecha_publi,isbn)
+            system('cls')
+            console.print(f'Libro añadido {nombre} correctamente')
+            input("Pulsa enter para continuar")
+            system('cls')
+        else:
+            console.input('El libro ya esta añadido en la lista')
 
-    # Funcion para agregar clientes
+    #Funcion para prestar libro ---------------------------------------------------------------------------------------------------
+    def prestar_libro(self,dni,nombre,nombre_libro, fecha_prestamo, fecha_devolucion):
+
+        self.prestados[dni] = Prestamo(dni,nombre,nombre_libro,fecha_prestamo,fecha_devolucion)
+        print(self.prestados)
+        input()
+        # del self.libros[nombre]
+
+    # Funcion para agregar clientes ----------------------------------------------------------------------------------------------
     def agregar_cliente(self,dni,nombre,fecha_nac,tlf,correo_electronico):
         if dni not in self.clientes:
             self.clientes[dni] = Cliente(dni,nombre,fecha_nac,tlf,correo_electronico)
             system("cls")
-            input(f"Usuario {nombre} registrado correctamente")
+            console.print(f"Usuario {nombre} registrado correctamente")
             input("Pulsa enter para continuar")
             system("cls")
 
-    # Funcion para mostar clientes
-    def mostra_clientes(self):
+    # Funcion para mostar clientes ------------------------------------------------------------------------------------------------
+    def mostrar_clientes(self):
         for cliente in self.clientes.values():
             console.print(f"Dni {cliente.dni}\t Nombre{cliente.nombre}")
         input()
-            
-
     
-
-
 console = Console()
 console.input('Poner en pantalla completa para visualizar correctamete')
 system("cls")
@@ -130,10 +159,6 @@ menu_principal = Panel(
 #Instanciamos la clase bibloteca
 arcana = Biblioteca()
 
-
-
-
-
 # Bucle principal
 def main():
     console.print(titulo)
@@ -142,27 +167,35 @@ def main():
         console.print(menu_principal)
         
         # Pregutamos que quiere hacer
-        opcion = int(input('Seleciona una opcion'))
+        opcion = int(input('Seleciona una opcion: '))
 
         if opcion == 1: # Opcion de añadir libro --------------------------------------------------------------------------------------------
             while True:
                 system('cls') # Borramos pantalla
-
-                console.print('Añadir libro')# Le mostramos en que menu estamos
-                
-                # Le pedimos todos los datos necesarios
-                nombre_libro = input('Introduce el nombre del libro: ')
-                editorail = input('Intrpduce la editorial del libro: ')
-                autor = input('Intrpduce el autor del libro: ')
-                fecha_publi = input('Introduce la fecha de salida del libro: ')
-                isbn = input('Introduce el ISBN: ')
-
-                # Instanciamos una clase 
-                
+                try:
+                    console.print('Añadir libro')# Le mostramos en que menu estamos
+                    # Le pedimos todos los datos necesarios
+                    nombre_libro = input('Introduce el nombre del libro: ')
+                    editorial = input('Introduce la editorial del libro: ')
+                    autor = input('Introduce el autor del libro: ')
+                    fecha_publi = input('Introduce la fecha de salida del libro: ')
+                    isbn = input('Introduce el ISBN: ')
+                    arcana.añadir_libro(nombre_libro,editorial,autor,fecha_publi,isbn)
+                    break
+                except:
+                    console.input('Has introducido algun dato erroneo\n\nPulsa enter para continuar')
+                    system('cls')
+                    continue
                 
 
         elif opcion == 2: # Opcion de prestar libro -----------------------------------------------------------------------------------------
             console.print('Prestar libro') 
+            dni = prompt('Introduce el dni del clienre para prestar: ')
+            nombre = prompt('Introduce el nombre del cliente')
+            nombre_libro = prompt('Introduce el nombre del libro a prestar')
+            fecha_prestamo = prompt('Introduce la fecha del prestamo')
+            fecha_devolucion = prompt('Introduce la fecha de devolucion')
+            arcana.prestar_libro(dni,nombre,nombre_libro, fecha_prestamo, fecha_devolucion)
 
         elif opcion == 3: # Opcion de devolver libro  ---------------------------------------------------------------------------------------
             console.print('Devolver libro') 
@@ -198,7 +231,7 @@ def main():
             arcana.agregar_cliente(dni,nombre,fecha_nac,tlf,correo_electronico)
         elif opcion == 7: # Mostrar usuarios registrados  ------------------------------------------------------------------------------------
             console.print('Mostar usuarios registrados')
-            arcana.mostra_clientes()
+            arcana.mostrar_clientes()
             input()
         elif opcion == 8: # Eliminar usuario  -----------------------------------------------------------------------------------------
             console.print('Eliminar usuario')
